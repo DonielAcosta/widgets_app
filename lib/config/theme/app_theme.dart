3a71bfad4615a968b75c8654e528c5cb52e1e20e
Doniel Acosta <donielacosta1995@gmail.com>
2026-05-24 08:31:04 -0400

    cambios para que use colores

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.yellow,
+  Colors.purple,
+  Colors.orange,
+  Colors.brown,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+    : assert(selectedColor >= 0, 'Selected color must be less than 7'),
+      assert(
+        selectedColor < colorList.length,
+        'Selected color must be less than ${colorList.length - 1}',
+      );
+
+  ThemeData getTheme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: colorList[selectedColor]);
+}
